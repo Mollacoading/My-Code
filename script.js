5923b16fe9f74e7c4aa936050cd70728d78135ad
Mollacoading <ashimmolla25@gmail.com>
2024-02-03 20:43:34 +0600

    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-document.getElementById('one').innerHTML=;
+let textView= document.querySelector("#one-1");
+
+let btn = document.querySelector("#one");
+
+
+let imranInfo = {
+    name: "Imran",
+    age: 17,
+    lastName: "Hossain",
+    fullName: function(){
+        return this.name+" "+this.lastName;
+    }
+}
+
+const imranFullName = imranInfo.fullName();
+
+textView.innerHTML = imranFullName;
+
+
+
+
+
+
